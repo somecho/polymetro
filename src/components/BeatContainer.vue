239a4b471dfba969d6240fed6a69ff86e31eb509
Samuel Cho <samuelcho@gmx.de>
2020-03-15 13:48:47 +0100

    add beat element

--- src/components/BeatContainer.vue
+++ src/components/BeatContainer.vue
@@ -1,13 +1,22 @@
 <template>
-		<v-card outlined>
-				container
-				{{$store.state.metronome[index]}}
-		</v-card>
+	<v-card outlined>
+		<beat-element
+			v-for="(beat, beatIndex) in $store.state.metronome[index]
+				.accents"
+			:key="beatIndex"
+			:accent="beat"
+			></beat-element>
+	</v-card>
 </template>
 <script>
+import BeatElement from "@/components/BeatElement.vue";
+
 export default {
-		name: "beat-container",
-		props: ['index']
-}
+	name: "beat-container",
+	props: ["index"],
+	components: {
+		BeatElement
+	}
+};
 </script>
 <style scoped></style>
---
+++ src/components/BeatElement.vue
@@ -0,0 +1,13 @@
+<template>
+		<div>
+				{{accent}}
+				<slot></slot>
+		</div>
+</template>
+<script>
+export default {
+		name: "beat-element",
+		props: ['accent']
+}
+</script>
+<style scoped></style>
