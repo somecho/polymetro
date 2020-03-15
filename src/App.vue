c6f7f13e993f0ca3c10755b785ad9bf8b3b003fe
Samuel Cho <samuelcho@gmx.de>
2020-03-15 11:09:39 +0100

    add only available on mobile notice

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="app">
-		<router-view/>
-				<!-- BOTTOM NAVIGATION -->
-				<router-link to="/">POLYMETRO</router-link>
-				<router-link to="/about">ABOUT</router-link>
-  </div>
+	<div>
+		<div class="mobile-notice">{{ mobileNotice }}</div>
+		<div id="app">
+			<router-view />
+			<!-- BOTTOM NAVIGATION -->
+			<router-link to="/">POLYMETRO</router-link>
+			<router-link to="/about">ABOUT</router-link>
+		</div>
+	</div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+	name: "App",
+	data: () => ({
+		mobileNotice:
+			"This web app is only available on mobile. Try it out on your phone!"
+	})
+};
 </script>
 
 <style>
+@media (max-width: 600px) {
+	.mobile-notice {
+		display: none;
+	}
+}
+@media (min-width: 600px) {
+	#app {
+		display: none;
+	}
+}
 </style>
