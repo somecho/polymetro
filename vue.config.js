11aa53fde2c10e472dd073a01b69f3295fd10fee
Samuel Cho <samuelcho@gmx.de>
2020-03-16 11:55:25 +0100

    update PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+		pwa: {
+				name: 'Polymetro',
+				themeColor: '#00E5F',
+				appleMobileWebAppCapable: 'yes',
+		}
 }
